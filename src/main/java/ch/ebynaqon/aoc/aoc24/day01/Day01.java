ee238806c84975769774f86b1022cc7c0223f98d
Roland Bärtschi <roland.baertschi@gmail.com>
2024-12-01 06:45:34 +0100

    refactor day 1

--- src/main/java/ch/ebynaqon/aoc/aoc24/day01/Day01.java
+++ src/main/java/ch/ebynaqon/aoc/aoc24/day01/Day01.java
@@ -1,6 +1,7 @@
 package ch.ebynaqon.aoc.aoc24.day01;
 
 import ch.ebynaqon.aoc.helper.RawProblemInput;
+import ch.ebynaqon.aoc.helper.Statistics;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,8 +10,9 @@ interface Day01 {
 
     static ProblemInput parseProblem(RawProblemInput input) {
         List<String> lines = input.getLines();
-        List<Integer> left = new ArrayList<>(lines.size());
-        List<Integer> right = new ArrayList<>(lines.size());
+        int numberOfEntries = lines.size();
+        List<Integer> left = new ArrayList<>(numberOfEntries);
+        List<Integer> right = new ArrayList<>(numberOfEntries);
         for (String line : lines) {
             String[] parts = line.split("\\s+");
             left.add(Integer.parseInt(parts[0]));
@@ -21,24 +23,21 @@ interface Day01 {
 
     static long solvePart1(RawProblemInput input) {
         ProblemInput problem = parseProblem(input);
-        List<Integer> left = problem.left().stream().sorted().toList();
-        List<Integer> right = problem.right().stream().sorted().toList();
+        List<Integer> leftSorted = problem.left().stream().sorted().toList();
+        List<Integer> rightSorted = problem.right().stream().sorted().toList();
         long sumOfDistances = 0L;
-        for (int i = 0; i < left.size(); i++) {
-            sumOfDistances += Math.abs(left.get(i) - right.get(i));
+        for (int i = 0; i < leftSorted.size(); i++) {
+            sumOfDistances += Math.abs(leftSorted.get(i) - rightSorted.get(i));
         }
         return sumOfDistances;
     }
 
     static long solvePart2(RawProblemInput input) {
         ProblemInput problem = parseProblem(input);
-        List<Integer> left = problem.left();
-        List<Integer> right = problem.right();
-        long result = 0L;
-        for (Integer value : left) {
-            long occurrencesInRightList = right.stream().filter(value::equals).count();
-            result += value * occurrencesInRightList;
-        }
-        return result;
+        var rightStatistics = Statistics.entryCount(problem.right());
+        return problem.left().stream()
+                .mapToLong(value -> value * rightStatistics.getOrDefault(value, 0L))
+                .sum();
     }
+
 }
