baf47073b6e07342d43fef44a08da516458d6ffe
Roland Bärtschi <roland.baertschi@gmail.com>
2024-12-08 05:56:44 +0100

    prepare day 8

---
+++ src/main/java/ch/ebynaqon/aoc/aoc24/day08/Day08.java
@@ -0,0 +1,28 @@
+package ch.ebynaqon.aoc.aoc24.day08;
+
+import ch.ebynaqon.aoc.helper.RawProblemInput;
+
+import java.util.List;
+
+interface Day08 {
+
+    static ProblemInput parseProblem(RawProblemInput input) {
+        List<ProblemSample> samples = input.getLines().stream().map(Day08::parseLine).toList();
+        return new ProblemInput(samples);
+    }
+
+    private static ProblemSample parseLine(String input) {
+        return new ProblemSample(Long.parseLong(input));
+    }
+
+    static long solvePart1(RawProblemInput input) {
+        ProblemInput problem = parseProblem(input);
+        return problem.samples().stream().mapToLong(ProblemSample::value).min().orElseThrow();
+    }
+
+    static long solvePart2(RawProblemInput input) {
+        ProblemInput problem = parseProblem(input);
+        return problem.samples().stream().mapToLong(ProblemSample::value).max().orElseThrow();
+    }
+}
+
---
+++ src/main/java/ch/ebynaqon/aoc/aoc24/day08/ProblemSample.java
@@ -0,0 +1,5 @@
+package ch.ebynaqon.aoc.aoc24.day08;
+
+record ProblemSample(long value) {
+}
+
