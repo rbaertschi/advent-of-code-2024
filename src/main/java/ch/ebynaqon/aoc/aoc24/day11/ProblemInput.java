a14948debb7c93ae75c263681c4ca7222dce6e1e
Roland Bärtschi <roland.baertschi@gmail.com>
2024-12-10 07:39:41 +0100

    prepare day 11

---
+++ src/main/java/ch/ebynaqon/aoc/aoc24/day11/ProblemInput.java
@@ -0,0 +1,7 @@
+package ch.ebynaqon.aoc.aoc24.day11;
+
+import java.util.List;
+
+record ProblemInput(List<ProblemSample> samples) {
+}
+
---
+++ src/main/java/ch/ebynaqon/aoc/aoc24/day11/ProblemSample.java
@@ -0,0 +1,5 @@
+package ch.ebynaqon.aoc.aoc24.day11;
+
+record ProblemSample(long value) {
+}
+
