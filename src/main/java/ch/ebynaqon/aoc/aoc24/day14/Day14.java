fecf25b7de6506260ab1c4aa0c98536a83d1e795
Roland Bärtschi <roland.baertschi@gmail.com>
2024-12-14 11:21:33 +0100

    ugly solution for part 2 of day 14

--- src/main/java/ch/ebynaqon/aoc/aoc24/day14/Day14.java
+++ src/main/java/ch/ebynaqon/aoc/aoc24/day14/Day14.java
@@ -2,9 +2,12 @@ package ch.ebynaqon.aoc.aoc24.day14;
 
 import ch.ebynaqon.aoc.helper.RawProblemInput;
 
+import java.util.ArrayList;
 import java.util.List;
 
 interface Day14 {
+
+    boolean PRINT_RESULT = false;
 
     static ProblemInput parseProblem(RawProblemInput input, int width, int height) {
         List<Robot> robots = input.getLines().stream().map(Day14::parseLine).toList();
@@ -23,11 +26,20 @@ interface Day14 {
 
     static int solvePart1(RawProblemInput input, int width, int height, int seconds) {
         ProblemInput problem = parseProblem(input, width, height);
+        ArrayList<Position> finalPositions = new ArrayList<>();
+        for (Robot robot : problem.robots()) {
+            Position finalPosition = move(robot.position(), robot.velocity(), seconds, width, height);
+            finalPositions.add(finalPosition);
+        }
+        int[] sectorCounts = getSectorCounts(finalPositions, width, height);
+        return sectorCounts[0] * sectorCounts[1] * sectorCounts[2] * sectorCounts[3];
+    }
+
+    private static int[] getSectorCounts(List<Position> positions, int width, int height) {
+        int[] sectorCounts = new int[4];
         int verticalLine = (width - 1) / 2;
         int horizontalLine = (height - 1) / 2;
-        int[] sectorCounts = new int[4];
-        for (Robot robot : problem.robots()) {
-            Position finalPosition = move(robot, seconds, width, height);
+        for (Position finalPosition : positions) {
             if (finalPosition.x() < verticalLine) {
                 if (finalPosition.y() < horizontalLine) {
                     // top left
@@ -46,12 +58,10 @@ interface Day14 {
                 }
             }
         }
-        return sectorCounts[0] * sectorCounts[1] * sectorCounts[2] * sectorCounts[3];
+        return sectorCounts;
     }
 
-    static Position move(Robot robot, int seconds, int width, int height) {
-        Position position = robot.position();
-        Velocity velocity = robot.velocity();
+    static Position move(Position position, Velocity velocity, int seconds, int width, int height) {
         for (int i = 0; i < seconds; i++) {
             position = position.moveWithTeleportation(velocity, width, height);
         }
@@ -60,7 +70,83 @@ interface Day14 {
 
     static long solvePart2(RawProblemInput input, int width, int height) {
         ProblemInput problem = parseProblem(input, width, height);
-        return 0;
+        List<Position> robotPositions = new ArrayList<>(problem.robots().stream().map(Robot::position).toList());
+        int seconds = 0;
+        while (true) {
+            seconds++;
+            for (int i = 0; i < robotPositions.size(); i++) {
+                Position nextPosition = move(robotPositions.get(i), problem.robots().get(i).velocity(), 1, width, height);
+                robotPositions.set(i, nextPosition);
+            }
+            int stats = numberOfMirroredPoints(robotPositions, width, height);
+            if (stats > 200) {
+                if (PRINT_RESULT) {
+                    System.out.println("========== Seconds: " + seconds + " ==========");
+                    System.out.println(stats);
+                    print(robotPositions, width, height);
+                }
+                return seconds;
+            }
+        }
+    }
+
+    static int numberOfMirroredPoints(List<Position> robotPositions, int width, int height) {
+        List<List<Integer>> xValuesByRow = new ArrayList<>(height);
+        for (int i = 0; i < height; i++) {
+            xValuesByRow.add(new ArrayList<>());
+        }
+        for (Position position : robotPositions) {
+            xValuesByRow.get(position.y()).add(position.x());
+        }
+        int[] averages = new int[height];
+        int average = 0;
+        for (int y = 0; y < height; y++) {
+            List<Integer> xValues = xValuesByRow.get(y);
+            int sum = 0;
+            int count = 0;
+            for (int x : xValues) {
+                sum += x;
+                count++;
+            }
+            if (count > 0) {
+                averages[y] = sum / count;
+                average += averages[y];
+            }
+        }
+        average /= height;
+        int mirroredPoints = 0;
+        for (int y = 0; y < height; y++) {
+            List<Integer> xs = xValuesByRow.get(y);
+            for (int x : xs) {
+                int mirrorX = average + (average - x);
+                for (int otherX : xs) {
+                    if (x != otherX && Math.abs(otherX - mirrorX) < 1) {
+                        mirroredPoints++;
+                    }
+                }
+            }
+        }
+
+        return mirroredPoints;
+    }
+
+    static void print(List<Position> positions, int width, int height) {
+        int[][] counts = new int[width][height];
+        for (Position position : positions) {
+            counts[position.x()][position.y()]++;
+        }
+        StringBuilder b = new StringBuilder(width * height);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int count = counts[x][y];
+                if (count > 0) {
+                    b.append("x");
+                } else {
+                    b.append(" ");
+                }
+            }
+            b.append("\n");
+        }
+        System.out.println(b);
     }
 }
-
--- src/test/java/ch/ebynaqon/aoc/aoc24/day14/Day14Test.java
+++ src/test/java/ch/ebynaqon/aoc/aoc24/day14/Day14Test.java
@@ -1,7 +1,6 @@
 package ch.ebynaqon.aoc.aoc24.day14;
 
 import ch.ebynaqon.aoc.helper.RawProblemInput;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -27,11 +26,11 @@ class Day14Test {
 
         // then
         assertThat(actual).isEqualTo(new ProblemInput(List.of(
-                new Robot(new Position(0,4), new Velocity(3,-3)),
-                new Robot(new Position(6,3), new Velocity(-1,-3)),
-                new Robot(new Position(10,3), new Velocity(-1,2)),
-                new Robot(new Position(2,0), new Velocity(2,-1)),
-                new Robot(new Position(0,0), new Velocity(1,3))
+                new Robot(new Position(0, 4), new Velocity(3, -3)),
+                new Robot(new Position(6, 3), new Velocity(-1, -3)),
+                new Robot(new Position(10, 3), new Velocity(-1, 2)),
+                new Robot(new Position(2, 0), new Velocity(2, -1)),
+                new Robot(new Position(0, 0), new Velocity(1, 3))
         ), 11, 7));
     }
 
@@ -73,33 +72,6 @@ class Day14Test {
     }
 
     @Test
-    @Disabled
-    void solvePart2UsingExample() {
-        // given
-        RawProblemInput input = new RawProblemInput("""
-                p=0,4 v=3,-3
-                p=6,3 v=-1,-3
-                p=10,3 v=-1,2
-                p=2,0 v=2,-1
-                p=0,0 v=1,3
-                p=3,0 v=-2,-2
-                p=7,6 v=-1,-3
-                p=3,0 v=-1,-2
-                p=9,3 v=2,3
-                p=7,3 v=-1,2
-                p=2,4 v=2,-3
-                p=9,5 v=-3,-3
-                """);
-
-        // when
-        var result = Day14.solvePart2(input, 11, 7);
-
-        // then
-        assertThat(result).isEqualTo(42);
-    }
-
-    @Test
-    @Disabled
     void solvePart2() {
         // given
         RawProblemInput input = RawProblemInput.fromResource("/day14.txt");
@@ -108,7 +80,7 @@ class Day14Test {
         var result = Day14.solvePart2(input, 101, 103);
 
         // then
-        assertThat(result).isEqualTo(42);
+        assertThat(result).isEqualTo(7055);
     }
 
 }
