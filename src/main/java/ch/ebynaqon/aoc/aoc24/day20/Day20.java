faf1edbb14c4cf2980a4f41fc9d515b4cbb9dca7
Roland Bärtschi <roland.baertschi@gmail.com>
2024-12-20 07:27:42 +0100

    refactoring day 20

--- src/main/java/ch/ebynaqon/aoc/aoc24/day20/Day20.java
+++ src/main/java/ch/ebynaqon/aoc/aoc24/day20/Day20.java
@@ -27,60 +27,58 @@ interface Day20 {
         return new ProblemInput(obstacles, start, end, rows, cols);
     }
 
-    static long solvePart1(RawProblemInput input, int minPicoSecondsSaved) {
+    static long solvePart1(RawProblemInput input, int timeSavedThreshold) {
         ProblemInput problem = parseProblem(input);
         List<TrackPosition> track = computeRaceTrack(problem);
-        List<Cheat> cheats = findCheats(2, minPicoSecondsSaved, track);
-        return cheats.size();
+        return countCheats(2, timeSavedThreshold, track);
     }
 
-    private static List<Cheat> findCheats(int maxCheatTime, int minPicoSecondsSaved, List<TrackPosition> track) {
-        List<Cheat> cheats = new ArrayList<>();
-        for (int i = 0; i < track.size() - 1; i++) {
-            TrackPosition from = track.get(i);
-            for (int j = i + 1; j < track.size(); j++) {
-                TrackPosition to = track.get(j);
-                int distance = from.distance(to);
-                if (distance <= maxCheatTime) {
-                    int timeSaved = from.distance() - (to.distance() + distance);
-                    if (timeSaved >= minPicoSecondsSaved) {
-                        cheats.add(new Cheat(from.position(), to.position(), timeSaved));
-                    }
-                }
-            }
-        }
-        return cheats;
-    }
-
-    static long solvePart2(RawProblemInput input, int minPicoSecondsSaved) {
+    static long solvePart2(RawProblemInput input, int timeSavedThreshold) {
         ProblemInput problem = parseProblem(input);
         List<TrackPosition> track = computeRaceTrack(problem);
-        List<Cheat> cheats = findCheats(20, minPicoSecondsSaved, track);
-        return cheats.size();
+        return countCheats(20, timeSavedThreshold, track);
     }
 
     static List<TrackPosition> computeRaceTrack(ProblemInput problem) {
         boolean[][] visited = new boolean[problem.rows()][problem.cols()];
-        List<TrackPosition> track = new ArrayList<>();
+        List<TrackPosition> reverseTrack = new ArrayList<>();
         TrackPosition current = new TrackPosition(problem.end(), 0);
         visited[current.position().row()][current.position().col()] = true;
-        track.add(current);
+        reverseTrack.add(current);
         while (!current.position().equals(problem.start())) {
             TrackPosition last = current;
             for (Position nextPosition : current.position().neighbours()) {
                 if (!problem.isObstructed(nextPosition) && !visited[nextPosition.row()][nextPosition.col()]) {
-                    TrackPosition next = new TrackPosition(nextPosition, current.distance() + 1);
-                    track.add(next);
+                    TrackPosition next = new TrackPosition(nextPosition, current.timeToEnd() + 1);
+                    reverseTrack.add(next);
                     current = next;
                     visited[nextPosition.row()][nextPosition.col()] = true;
                     break;
                 }
             }
             if (current.equals(last)) {
-                throw new IllegalStateException("Could not find path forward!");
+                throw new IllegalStateException("Stuck at the same location! Could not find path forward!");
             }
         }
-        return track.reversed();
+        return reverseTrack.reversed();
+    }
+
+    private static int countCheats(int maxCheatDuration, int timeSavedThreshold, List<TrackPosition> track) {
+        int numberOfCheats = 0;
+        for (int i = 0; i < track.size() - 1; i++) {
+            TrackPosition cheatStart = track.get(i);
+            for (int j = i + 1; j < track.size(); j++) {
+                TrackPosition cheatEnd = track.get(j);
+                int durationOfCheat = cheatStart.distance(cheatEnd);
+                if (durationOfCheat <= maxCheatDuration) {
+                    int timeSaved = cheatStart.timeToEnd() - (cheatEnd.timeToEnd() + durationOfCheat);
+                    if (timeSaved >= timeSavedThreshold) {
+                        numberOfCheats++;
+                    }
+                }
+            }
+        }
+        return numberOfCheats;
     }
 }
 
@@ -91,13 +89,7 @@ record ProblemInput(boolean[][] obstacles, Position start, Position end, int row
     }
 }
 
-record Cheat(Position start, Position end, int gain) {
-}
-
-record RaceTrack(int[][] distances, List<Position> track) {
-}
-
-record TrackPosition(Position position, int distance) {
+record TrackPosition(Position position, int timeToEnd) {
     public int distance(TrackPosition to) {
         return position.distance(to.position());
     }
