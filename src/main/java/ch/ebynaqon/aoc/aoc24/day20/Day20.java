e6fd5019dbe5e7a638462177cb2dcd881355fada
Roland Bärtschi <roland.baertschi@gmail.com>
2024-12-20 07:06:13 +0100

    solve part 1 of day 20

--- src/main/java/ch/ebynaqon/aoc/aoc24/day20/Day20.java
+++ src/main/java/ch/ebynaqon/aoc/aoc24/day20/Day20.java
@@ -2,35 +2,111 @@ package ch.ebynaqon.aoc.aoc24.day20;
 
 import ch.ebynaqon.aoc.helper.RawProblemInput;
 
+import java.util.ArrayList;
 import java.util.List;
 
 interface Day20 {
 
     static ProblemInput parseProblem(RawProblemInput input) {
-        List<ProblemSample> samples = input.getLines().stream().map(Day20::parseLine).toList();
-        return new ProblemInput(samples);
+        List<String> lines = input.getLines();
+        int rows = lines.size();
+        int cols = lines.getFirst().length();
+        boolean[][] obstacles = new boolean[rows][cols];
+        Position start = null, end = null;
+        for (int row = 0; row < rows; row++) {
+            String line = lines.get(row);
+            for (int col = 0; col < cols; col++) {
+                switch (line.charAt(col)) {
+                    case '#' -> obstacles[row][col] = true;
+                    case 'S' -> start = new Position(row, col);
+                    case 'E' -> end = new Position(row, col);
+                    default -> { /* free space */ }
+                }
+            }
+        }
+        return new ProblemInput(obstacles, start, end, rows, cols);
     }
 
-    private static ProblemSample parseLine(String input) {
-        return new ProblemSample(Long.parseLong(input));
-    }
-
-    static long solvePart1(RawProblemInput input) {
+    static long solvePart1(RawProblemInput input, int minPicoSecondsSaved) {
         ProblemInput problem = parseProblem(input);
-        return problem.samples().stream().mapToLong(ProblemSample::value).min().orElseThrow();
+        List<TrackPosition> track = computeRaceTrack(problem);
+        List<Cheat> cheats = new ArrayList<>();
+        for (int i = 0; i < track.size() - 1; i++) {
+            TrackPosition from = track.get(i);
+            for (int j = i + 1; j < track.size(); j++) {
+                TrackPosition to = track.get(j);
+                int distance = from.distance(to);
+                if (distance <= 2) {
+                    int timeSaved = from.distance() - (to.distance() + distance);
+                    if (timeSaved >= minPicoSecondsSaved) {
+                        cheats.add(new Cheat(from.position(), to.position(), timeSaved));
+                    }
+                }
+            }
+        }
+        return cheats.size();
+    }
+
+    static List<TrackPosition> computeRaceTrack(ProblemInput problem) {
+        boolean[][] visited = new boolean[problem.rows()][problem.cols()];
+        List<TrackPosition> track = new ArrayList<>();
+        TrackPosition current = new TrackPosition(problem.end(), 0);
+        visited[current.position().row()][current.position().col()] = true;
+        track.add(current);
+        while (!current.position().equals(problem.start())) {
+            TrackPosition last = current;
+            for (Position nextPosition : current.position().neighbours()) {
+                if (!problem.isObstructed(nextPosition) && !visited[nextPosition.row()][nextPosition.col()]) {
+                    TrackPosition next = new TrackPosition(nextPosition, current.distance() + 1);
+                    track.add(next);
+                    current = next;
+                    visited[nextPosition.row()][nextPosition.col()] = true;
+                    break;
+                }
+            }
+            if (current.equals(last)) {
+                throw new IllegalStateException("Could not find path forward!");
+            }
+        }
+        return track.reversed();
     }
 
     static long solvePart2(RawProblemInput input) {
         ProblemInput problem = parseProblem(input);
-        return problem.samples().stream().mapToLong(ProblemSample::value).max().orElseThrow();
+        return 0;
     }
 }
 
 
-record ProblemInput(List<ProblemSample> samples) {
+record ProblemInput(boolean[][] obstacles, Position start, Position end, int rows, int cols) {
+    public boolean isObstructed(Position position) {
+        return obstacles[position.row()][position.col()];
+    }
 }
 
-
-record ProblemSample(long value) {
+record Cheat(Position start, Position end, int gain) {
 }
 
+record RaceTrack(int[][] distances, List<Position> track) {
+}
+
+record TrackPosition(Position position, int distance) {
+    public int distance(TrackPosition to) {
+        return position.distance(to.position());
+    }
+}
+
+record Position(int row, int col) {
+    public List<Position> neighbours() {
+        return List.of(
+                new Position(row + 1, col),
+                new Position(row - 1, col),
+                new Position(row, col + 1),
+                new Position(row, col - 1)
+        );
+    }
+
+    public int distance(Position to) {
+        return Math.abs(to.row() - row) + Math.abs(to.col() - col);
+    }
+}
--- src/test/java/ch/ebynaqon/aoc/aoc24/day20/Day20Test.java
+++ src/test/java/ch/ebynaqon/aoc/aoc24/day20/Day20Test.java
@@ -3,8 +3,11 @@ package ch.ebynaqon.aoc.aoc24.day20;
 import ch.ebynaqon.aoc.helper.RawProblemInput;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,44 +18,85 @@ class Day20Test {
     void parseProblemInput() {
         // given
         RawProblemInput input = new RawProblemInput("""
-                42
+                #####
+                ###E#
+                #S# #
+                # # #
+                #   #
+                #####
                 """);
 
         // when
         var actual = Day20.parseProblem(input);
 
         // then
-        assertThat(actual).isEqualTo(new ProblemInput(List.of(
-                new ProblemSample(42L)
-        )));
+        assertThat(actual.obstacles()).isEqualTo(new boolean[][]{
+                {true, true, true, true, true},
+                {true, true, true, false, true},
+                {true, false, true, false, true},
+                {true, false, true, false, true},
+                {true, false, false, false, true},
+                {true, true, true, true, true}
+        });
+        assertThat(actual.start()).isEqualTo(new Position(2, 1));
+        assertThat(actual.end()).isEqualTo(new Position(1, 3));
     }
 
-    @Test
-    @Disabled
-    void solvePart1UsingExample() {
+    @ParameterizedTest
+    @MethodSource("cheats")
+    void solvePart1UsingExample(int atLeastTimeSaved, int numberOfCheats) {
         // given
         RawProblemInput input = new RawProblemInput("""
-                42
+                ###############
+                #...#...#.....#
+                #.#.#.#.#.###.#
+                #S#...#.#.#...#
+                #######.#.#.###
+                #######.#.#...#
+                #######.#.###.#
+                ###..E#...#...#
+                ###.#######.###
+                #...###...#...#
+                #.#####.#.###.#
+                #.#...#.#.#...#
+                #.#.#.#.#.#.###
+                #...#...#...###
+                ###############
                 """);
 
         // when
-        var result = Day20.solvePart1(input);
+        var result = Day20.solvePart1(input, atLeastTimeSaved);
 
         // then
-        assertThat(result).isEqualTo(42);
+        assertThat(result).isEqualTo(numberOfCheats);
+    }
+
+    public static Stream<Arguments> cheats() {
+        return Stream.of(
+                Arguments.arguments(2,  44),
+                Arguments.arguments(4,  30),
+                Arguments.arguments(6,  16),
+                Arguments.arguments(8,  14),
+                Arguments.arguments(10,  10),
+                Arguments.arguments(12,  8),
+                Arguments.arguments(20,  5),
+                Arguments.arguments(36,  4),
+                Arguments.arguments(38,  3),
+                Arguments.arguments(40,  2),
+                Arguments.arguments(64,  1)
+        );
     }
 
     @Test
-    @Disabled
     void solvePart1() {
         // given input from https://adventofcode.com/2024/day/20/input
         RawProblemInput input = RawProblemInput.fromResource("/day20.txt");
 
         // when
-        var result = Day20.solvePart1(input);
+        var result = Day20.solvePart1(input, 100);
 
         // then
-        assertThat(result).isEqualTo(42);
+        assertThat(result).isEqualTo(1409);
     }
 
     @Test
