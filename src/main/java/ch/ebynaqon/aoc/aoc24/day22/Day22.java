10291a37b7ef23efe7d11c786aa9fbb0ffdffa89
Roland Bärtschi <roland.baertschi@gmail.com>
2024-12-22 06:30:47 +0100

    solve part 1 of day 22

--- src/main/java/ch/ebynaqon/aoc/aoc24/day22/Day22.java
+++ src/main/java/ch/ebynaqon/aoc/aoc24/day22/Day22.java
@@ -7,28 +7,74 @@ import java.util.List;
 interface Day22 {
 
     static ProblemInput parseProblem(RawProblemInput input) {
-        List<ProblemSample> samples = input.getLines().stream().map(Day22::parseLine).toList();
+        List<SecretNumber> samples = input.getLines().stream().map(Day22::parseLine).toList();
         return new ProblemInput(samples);
     }
 
-    private static ProblemSample parseLine(String input) {
-        return new ProblemSample(Long.parseLong(input));
+    private static SecretNumber parseLine(String input) {
+        return new SecretNumber(Long.parseLong(input));
     }
 
     static long solvePart1(RawProblemInput input) {
         ProblemInput problem = parseProblem(input);
-        return problem.samples().stream().mapToLong(ProblemSample::value).min().orElseThrow();
+        return problem.samples().stream()
+                .map(Day22::get2000th)
+                .mapToLong(SecretNumber::value).sum();
     }
 
     static long solvePart2(RawProblemInput input) {
         ProblemInput problem = parseProblem(input);
-        return problem.samples().stream().mapToLong(ProblemSample::value).max().orElseThrow();
+        return problem.samples().stream().mapToLong(SecretNumber::value).max().orElseThrow();
     }
+
+    private static SecretNumber get2000th(SecretNumber secret) {
+        SecretNumber result = secret;
+        for (int i = 0; i < 2000; i++) {
+            result = result.next();
+        }
+        return result;
+    }
+
 }
 
-record ProblemInput(List<ProblemSample> samples) {
+record ProblemInput(List<SecretNumber> samples) {
 }
 
-record ProblemSample(long value) {
+record SecretNumber(long value) {
+    SecretNumber next() {
+        /*
+        - Calculate the result of multiplying the secret number by 64.
+          Then, mix this result into the secret number.
+          Finally, prune the secret number.
+        - Calculate the result of dividing the secret number by 32.
+          Round the result down to the nearest integer.
+          Then, mix this result into the secret number.
+          Finally, prune the secret number.
+        - Calculate the result of multiplying the secret number by 2048.
+          Then, mix this result into the secret number.
+          Finally, prune the secret number.
+
+        Each step of the above process involves mixing and pruning:
+        - To mix a value into the secret number, calculate the bitwise XOR of the given value and the secret number.
+          Then, the secret number becomes the result of that operation.
+          (If the secret number is 42 and you were to mix 15 into the secret number, the secret number would become 37.)
+        - To prune the secret number, calculate the value of the secret number modulo 16777216.
+          Then, the secret number becomes the result of that operation.
+          (If the secret number is 100000000 and you were to prune the secret number, the secret number would become 16113920.)
+        */
+        long secret = value;
+        secret = prune(mix(secret * 64, secret));
+        secret = prune(mix(secret / 32, secret));
+        secret = prune(mix(secret * 2048, secret));
+        return new SecretNumber(secret);
+    }
+
+    private long prune(long secret) {
+        return secret % 16777216;
+    }
+
+    private long mix(long value, long secret) {
+        return value ^ secret;
+    }
 }
 
--- src/test/java/ch/ebynaqon/aoc/aoc24/day22/Day22Test.java
+++ src/test/java/ch/ebynaqon/aoc/aoc24/day22/Day22Test.java
@@ -3,8 +3,12 @@ package ch.ebynaqon.aoc.aoc24.day22;
 import ch.ebynaqon.aoc.helper.RawProblemInput;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,7 +19,10 @@ class Day22Test {
     void parseProblemInput() {
         // given
         RawProblemInput input = new RawProblemInput("""
-                42
+                15887950
+                16495136
+                527345
+                704524
                 """);
 
         // when
@@ -23,27 +30,31 @@ class Day22Test {
 
         // then
         assertThat(actual).isEqualTo(new ProblemInput(List.of(
-                new ProblemSample(42L)
+                new SecretNumber(15887950),
+                new SecretNumber(16495136),
+                new SecretNumber(527345),
+                new SecretNumber(704524)
         )));
     }
 
     @Test
-    @Disabled
     void solvePart1UsingExample() {
         // given
         RawProblemInput input = new RawProblemInput("""
-                42
+                1
+                10
+                100
+                2024
                 """);
 
         // when
         var result = Day22.solvePart1(input);
 
         // then
-        assertThat(result).isEqualTo(42);
+        assertThat(result).isEqualTo(37327623);
     }
 
     @Test
-    @Disabled
     void solvePart1() {
         // given input from https://adventofcode.com/2024/day/22/input
         RawProblemInput input = RawProblemInput.fromResource("/day22.txt");
@@ -52,7 +63,7 @@ class Day22Test {
         var result = Day22.solvePart1(input);
 
         // then
-        assertThat(result).isEqualTo(42);
+        assertThat(result).isEqualTo(20506453102L);
     }
 
     @Test
@@ -83,5 +94,45 @@ class Day22Test {
         assertThat(result).isEqualTo(42);
     }
 
+    @ParameterizedTest
+    @MethodSource("secretNumbers2000")
+    void secretGeneration2000(long initialSecret, long secret2000) {
+        SecretNumber secret = new SecretNumber(initialSecret);
+
+        for (int i = 0; i < 2000; i++) {
+            secret = secret.next();
+        }
+
+        assertThat(secret).isEqualTo(new SecretNumber(secret2000));
+    }
+
+    public static Stream<Arguments> secretNumbers2000() {
+        return Stream.of(
+                Arguments.arguments(1, 8685429),
+                Arguments.arguments(10, 4700978),
+                Arguments.arguments(100, 15273692),
+                Arguments.arguments(2024, 8667524)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("secretNumbers")
+    void secretGeneration(long current, long next) {
+        assertThat(new SecretNumber(current).next()).isEqualTo(new SecretNumber(next));
+    }
+
+    public static Stream<Arguments> secretNumbers() {
+        return Stream.of(
+                Arguments.arguments(15887950, 16495136),
+                Arguments.arguments(16495136, 527345),
+                Arguments.arguments(527345, 704524),
+                Arguments.arguments(704524, 1553684),
+                Arguments.arguments(1553684, 12683156),
+                Arguments.arguments(12683156, 11100544),
+                Arguments.arguments(11100544, 12249484),
+                Arguments.arguments(12249484, 7753432),
+                Arguments.arguments(7753432, 5908254)
+        );
+    }
 }
 
