734d6213d3abe5e9e58b9dc33cfe557bdd13d110
Roland Bärtschi <roland.baertschi@gmail.com>
2024-12-23 06:21:32 +0100

    solve part 1 of day 23

--- src/main/java/ch/ebynaqon/aoc/aoc24/day23/Day23.java
+++ src/main/java/ch/ebynaqon/aoc/aoc24/day23/Day23.java
@@ -2,33 +2,71 @@ package ch.ebynaqon.aoc.aoc24.day23;
 
 import ch.ebynaqon.aoc.helper.RawProblemInput;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
 
 interface Day23 {
 
     static ProblemInput parseProblem(RawProblemInput input) {
-        List<ProblemSample> samples = input.getLines().stream().map(Day23::parseLine).toList();
+        List<Connection> samples = input.getLines().stream().map(Day23::parseLine).toList();
         return new ProblemInput(samples);
     }
 
-    private static ProblemSample parseLine(String input) {
-        return new ProblemSample(Long.parseLong(input));
+    private static Connection parseLine(String input) {
+        String[] parts = input.split("-");
+        return new Connection(parts[0], parts[1]);
     }
 
     static long solvePart1(RawProblemInput input) {
         ProblemInput problem = parseProblem(input);
-        return problem.samples().stream().mapToLong(ProblemSample::value).min().orElseThrow();
+        List<Connection> connections = problem.connections();
+        Map<String, List<String>> connectionsFrom = new HashMap<>();
+        Set<String> namesStartingWithT = new HashSet<>();
+        connections.forEach(connection -> {
+            connectionsFrom.computeIfAbsent(connection.from(), _ -> new ArrayList<>()).add(connection.to());
+            if (connection.from().startsWith("t")) {
+                namesStartingWithT.add(connection.from());
+            }
+            connectionsFrom.computeIfAbsent(connection.to(), _ -> new ArrayList<>()).add(connection.from());
+            if (connection.to().startsWith("t")) {
+                namesStartingWithT.add(connection.to());
+            }
+        });
+        Set<ComputerTriple> result = new HashSet<>();
+        for (String first : namesStartingWithT) {
+            List<String> connectedToFirst = connectionsFrom.get(first);
+            for (String second : connectedToFirst) {
+                for (String third : connectionsFrom.get(second)) {
+                    if (!third.equals(first) && connectedToFirst.contains(third)) {
+                        result.add(ComputerTriple.from(first, second, third));
+                    }
+                }
+            }
+        }
+        return result.size();
     }
 
     static long solvePart2(RawProblemInput input) {
         ProblemInput problem = parseProblem(input);
-        return problem.samples().stream().mapToLong(ProblemSample::value).max().orElseThrow();
+        return 0;
     }
 }
 
-record ProblemInput(List<ProblemSample> samples) {
+record ComputerTriple(String first, String second, String third) {
+    static ComputerTriple from(String s1, String s2, String s3) {
+        List<String> sorted = Stream.of(s1, s2, s3).sorted().toList();
+        return new ComputerTriple(sorted.get(0), sorted.get(1), sorted.get(2));
+    }
 }
 
-record ProblemSample(long value) {
+record ProblemInput(List<Connection> connections) {
+}
+
+record Connection(String from, String to) {
 }
 
--- src/test/java/ch/ebynaqon/aoc/aoc24/day23/Day23Test.java
+++ src/test/java/ch/ebynaqon/aoc/aoc24/day23/Day23Test.java
@@ -15,7 +15,10 @@ class Day23Test {
     void parseProblemInput() {
         // given
         RawProblemInput input = new RawProblemInput("""
-                42
+                kh-tc
+                qp-kh
+                de-cg
+                ka-co
                 """);
 
         // when
@@ -23,27 +26,59 @@ class Day23Test {
 
         // then
         assertThat(actual).isEqualTo(new ProblemInput(List.of(
-                new ProblemSample(42L)
+                new Connection("kh","tc"),
+                new Connection("qp","kh"),
+                new Connection("de","cg"),
+                new Connection("ka","co")
         )));
     }
 
     @Test
-    @Disabled
     void solvePart1UsingExample() {
         // given
         RawProblemInput input = new RawProblemInput("""
-                42
+                kh-tc
+                qp-kh
+                de-cg
+                ka-co
+                yn-aq
+                qp-ub
+                cg-tb
+                vc-aq
+                tb-ka
+                wh-tc
+                yn-cg
+                kh-ub
+                ta-co
+                de-co
+                tc-td
+                tb-wq
+                wh-td
+                ta-ka
+                td-qp
+                aq-cg
+                wq-ub
+                ub-vc
+                de-ta
+                wq-aq
+                wq-vc
+                wh-yn
+                ka-de
+                kh-ta
+                co-tc
+                wh-qp
+                tb-vc
+                td-yn
                 """);
 
         // when
         var result = Day23.solvePart1(input);
 
         // then
-        assertThat(result).isEqualTo(42);
+        assertThat(result).isEqualTo(7);
     }
 
     @Test
-    @Disabled
     void solvePart1() {
         // given input from https://adventofcode.com/2024/day/23/input
         RawProblemInput input = RawProblemInput.fromResource("/day23.txt");
@@ -52,7 +87,7 @@ class Day23Test {
         var result = Day23.solvePart1(input);
 
         // then
-        assertThat(result).isEqualTo(42);
+        assertThat(result).isEqualTo(1098);
     }
 
     @Test
