5988913c2841636be577e611778ac90ca6a590b7
Roland Bärtschi <roland.baertschi@gmail.com>
2024-12-24 06:38:04 +0100

    solve part 1 of day 24

--- src/main/java/ch/ebynaqon/aoc/aoc24/day24/Day24.java
+++ src/main/java/ch/ebynaqon/aoc/aoc24/day24/Day24.java
@@ -2,33 +2,114 @@ package ch.ebynaqon.aoc.aoc24.day24;
 
 import ch.ebynaqon.aoc.helper.RawProblemInput;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
 interface Day24 {
 
     static ProblemInput parseProblem(RawProblemInput input) {
-        List<ProblemSample> samples = input.getLines().stream().map(Day24::parseLine).toList();
-        return new ProblemInput(samples);
+        String[] initialWiresAndGates = input.getWholeInput().split("\\n\\n");
+        HashMap<String, WireState> initialWireStates = new HashMap<>();
+        for (String initialWire : initialWiresAndGates[0].split("\\n")) {
+            String[] nameAndState = initialWire.split(":\\s*");
+            initialWireStates.put(nameAndState[0], WireState.from(nameAndState[1]));
+        }
+        List<Gate> gates = Arrays.stream(initialWiresAndGates[1].split("\\n"))
+                .map(Day24::parseGate).toList();
+        return new ProblemInput(initialWireStates, gates);
     }
 
-    private static ProblemSample parseLine(String input) {
-        return new ProblemSample(Long.parseLong(input));
+    private static Gate parseGate(String input) {
+        String[] parts = input.split("\\s+");
+        return Gate.from(parts[1], parts[0], parts[2], parts[4]);
     }
 
     static long solvePart1(RawProblemInput input) {
         ProblemInput problem = parseProblem(input);
-        return problem.samples().stream().mapToLong(ProblemSample::value).min().orElseThrow();
+        HashMap<String, WireState> wireStates = new HashMap<>(problem.initialWireStates());
+        Queue<Gate> gatesToCompute = new LinkedList<>(problem.gates());
+        while (!gatesToCompute.isEmpty()) {
+            Gate gate = gatesToCompute.poll();
+            if (wireStates.containsKey(gate.input1()) && wireStates.containsKey(gate.input2())) {
+                WireState output = gate.compute(wireStates.get(gate.input1()), wireStates.get(gate.input2()));
+                wireStates.put(gate.output(), output);
+            } else {
+                gatesToCompute.add(gate);
+            }
+        }
+        List<Integer> outputNumbers = wireStates.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith("z"))
+                .sorted(Map.Entry.<String, WireState>comparingByKey().reversed())
+                .map(entry -> entry.getValue() == WireState.TRUE ? 1 : 0)
+                .toList();
+        long result = 0;
+        for (Integer outputNumber : outputNumbers) {
+            result = (result << 1) + outputNumber;
+        }
+        return result;
     }
 
     static long solvePart2(RawProblemInput input) {
         ProblemInput problem = parseProblem(input);
-        return problem.samples().stream().mapToLong(ProblemSample::value).max().orElseThrow();
+        return 0;
     }
 }
 
-record ProblemInput(List<ProblemSample> samples) {
+record ProblemInput(Map<String, WireState> initialWireStates, List<Gate> gates) {
 }
 
-record ProblemSample(long value) {
+interface Gate {
+    String input1();
+
+    String input2();
+
+    String output();
+
+    WireState compute(WireState stateInput1, WireState stateInput2);
+
+    static Gate from(String type, String input1, String input2, String output) {
+        return switch (type) {
+            case "AND" -> new AndGate(input1, input2, output);
+            case "XOR" -> new XorGate(input1, input2, output);
+            case "OR" -> new OrGate(input1, input2, output);
+            default -> throw new IllegalArgumentException("Unknown gate type: " + type);
+        };
+    }
 }
 
+record AndGate(String input1, String input2, String output) implements Gate {
+    @Override
+    public WireState compute(WireState stateInput1, WireState stateInput2) {
+        return stateInput1 == WireState.TRUE && stateInput2 == WireState.TRUE ? WireState.TRUE : WireState.FALSE;
+    }
+}
+
+record XorGate(String input1, String input2, String output) implements Gate {
+    @Override
+    public WireState compute(WireState stateInput1, WireState stateInput2) {
+        return stateInput1 != stateInput2 ? WireState.TRUE : WireState.FALSE;
+    }
+}
+
+record OrGate(String input1, String input2, String output) implements Gate {
+    @Override
+    public WireState compute(WireState stateInput1, WireState stateInput2) {
+        return stateInput1 == WireState.TRUE || stateInput2 == WireState.TRUE ? WireState.TRUE : WireState.FALSE;
+    }
+}
+
+enum WireState {
+    TRUE, FALSE, UNKNOWN;
+
+    public static WireState from(String input) {
+        return switch (input) {
+            case "1" -> TRUE;
+            case "0" -> FALSE;
+            default -> throw new IllegalArgumentException("Unknown wire state: " + input);
+        };
+    }
+}
--- src/test/java/ch/ebynaqon/aoc/aoc24/day24/Day24Test.java
+++ src/test/java/ch/ebynaqon/aoc/aoc24/day24/Day24Test.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,35 +16,121 @@ class Day24Test {
     void parseProblemInput() {
         // given
         RawProblemInput input = new RawProblemInput("""
-                42
+                x00: 1
+                x01: 1
+                x02: 1
+                y00: 0
+                y01: 1
+                y02: 0
+                
+                x00 AND y00 -> z00
+                x01 XOR y01 -> z01
+                x02 OR y02 -> z02
                 """);
 
         // when
         var actual = Day24.parseProblem(input);
 
         // then
-        assertThat(actual).isEqualTo(new ProblemInput(List.of(
-                new ProblemSample(42L)
-        )));
+        assertThat(actual).isEqualTo(new ProblemInput(Map.of(
+                "x00", WireState.TRUE,
+                "x01", WireState.TRUE,
+                "x02", WireState.TRUE,
+                "y00", WireState.FALSE,
+                "y01", WireState.TRUE,
+                "y02", WireState.FALSE
+        ),
+                List.of(
+                        new AndGate("x00", "y00", "z00"),
+                        new XorGate("x01", "y01", "z01"),
+                        new OrGate("x02", "y02", "z02")
+                )));
     }
 
     @Test
-    @Disabled
     void solvePart1UsingExample() {
         // given
         RawProblemInput input = new RawProblemInput("""
-                42
+                x00: 1
+                x01: 1
+                x02: 1
+                y00: 0
+                y01: 1
+                y02: 0
+                
+                x00 AND y00 -> z00
+                x01 XOR y01 -> z01
+                x02 OR y02 -> z02
                 """);
 
         // when
         var result = Day24.solvePart1(input);
 
         // then
-        assertThat(result).isEqualTo(42);
+        assertThat(result).isEqualTo(4);
     }
 
     @Test
-    @Disabled
+    void solvePart1UsingLargerExample() {
+        // given
+        RawProblemInput input = new RawProblemInput("""
+                x00: 1
+                x01: 0
+                x02: 1
+                x03: 1
+                x04: 0
+                y00: 1
+                y01: 1
+                y02: 1
+                y03: 1
+                y04: 1
+                
+                ntg XOR fgs -> mjb
+                y02 OR x01 -> tnw
+                kwq OR kpj -> z05
+                x00 OR x03 -> fst
+                tgd XOR rvg -> z01
+                vdt OR tnw -> bfw
+                bfw AND frj -> z10
+                ffh OR nrd -> bqk
+                y00 AND y03 -> djm
+                y03 OR y00 -> psh
+                bqk OR frj -> z08
+                tnw OR fst -> frj
+                gnj AND tgd -> z11
+                bfw XOR mjb -> z00
+                x03 OR x00 -> vdt
+                gnj AND wpb -> z02
+                x04 AND y00 -> kjc
+                djm OR pbm -> qhw
+                nrd AND vdt -> hwm
+                kjc AND fst -> rvg
+                y04 OR y02 -> fgs
+                y01 AND x02 -> pbm
+                ntg OR kjc -> kwq
+                psh XOR fgs -> tgd
+                qhw XOR tgd -> z09
+                pbm OR djm -> kpj
+                x03 XOR y03 -> ffh
+                x00 XOR y04 -> ntg
+                bfw OR bqk -> z06
+                nrd XOR fgs -> wpb
+                frj XOR qhw -> z04
+                bqk OR frj -> z07
+                y03 OR x01 -> nrd
+                hwm AND bqk -> z03
+                tgd XOR rvg -> z12
+                tnw OR pbm -> gnj
+                """);
+
+        // when
+        var result = Day24.solvePart1(input);
+
+        // then
+        assertThat(result).isEqualTo(2024);
+    }
+
+    @Test
     void solvePart1() {
         // given input from https://adventofcode.com/2024/day/24/input
         RawProblemInput input = RawProblemInput.fromResource("/day24.txt");
@@ -52,7 +139,7 @@ class Day24Test {
         var result = Day24.solvePart1(input);
 
         // then
-        assertThat(result).isEqualTo(42);
+        assertThat(result).isEqualTo(46362252142374L);
     }
 
     @Test
